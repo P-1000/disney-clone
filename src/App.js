1547acf470b390a31bfcd9f54543367a6ba2b6d0
p-1000 <patchikarla.12108354@lpu.in>
2023-03-06 23:45:47 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+  return (
+    <Container>
+      Header
+    </Container>
+  )
+}
+
+export default Header
+
+const Container = styled.div`
+    height: 70px;
+    background: #090b13;
+    `
