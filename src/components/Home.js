84789687d74ea16abfe188822b5dc25eea4e5d22
p-1000 <patchikarla.12108354@lpu.in>
2023-03-07 14:55:40 +0530

    starting with home page and slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Home() {
   return (
-    <div>
-      Home : 
-    </div>
+    <Container>
+      home 
+
+      
+    </Container>
   )
 }
 
 export default Home
+
+const Container = styled.main`
+  min-height : calc(100vh-70px);
+  positon:relative;
+  padding: calcu(3.5vw - 5px)
+  display:block;
+  overflow-x:hidden;
+
+
+    &:before{
+          background : url("./images/home-background.png") center center / cover no-repeat fixed;
+          content : "";
+          position : absolute;
+          inset:0px;
+          bottom : 0;
+          z-index:-1;
+
+    }
+
+`;
