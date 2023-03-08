00e3fa9f58ec20d4b8373de1e36052bb2ad3a4cf
p-1000 <patchikarla.12108354@lpu.in>
2023-03-09 05:09:33 +0530

    done

--- src/components/MyAc.js
+++ src/components/MyAc.js
@@ -4,6 +4,10 @@ import { Center, Container, Image, Text,Box} from "@chakra-ui/react";
 import { Card, CardHeader, CardBody, CardFooter ,Flex } from '@chakra-ui/react'
 import { selectUserName  , selectUserPhoto , setUserLogin, setUserLoginDetails , setSignOut} from '../features/user/userSlice'
 import { useDispatch, useSelector } from 'react-redux' 
+import {auth , provider} from '../features/firebase'
+import {useHistory} from 'react-router-dom'
+
+
 
 function MyAc() {
     const dispatch = useDispatch()
@@ -13,25 +17,67 @@ function MyAc() {
     <>
           <Center h="100vh" >
           <Container>
+          <Center>
           <Box>
-            <Image src={userPhoto} alt="Avatar" borderRadius="50%" boxSize="150px" mb="4" />
-            <Text fontSize="2xl" fontWeight="bold">{userName}</Text>
+            <Image src={userPhoto}  alt="Avatar" borderRadius="50%" boxSize="120px" />
+            <Center>
+            <Text fontSize="1.4em" textTransform="uppercase"  fontWeight="bold">{userName}</Text>
+            </Center>
           </Box>
+          </Center>
+        <Box className='cont'>
+          <Box  className='cards112' d="flex "  rowGap="5" alignItems="center" background="#192133" p="10px 10px 12px 12px" maxW="520px" mb="12px" w="980px">
+        <Box>
+          <Text fontSize="xl" fontWeight="semibold" >
+          Get more with Disney+ Hotstar Premium
+          <span d="block">Only ₹399/year</span>
+          </Text>
+            <Text fontSize="sm" color="#8D8D8D" >
+            Only ₹399/year
+            </Text>
+          </Box>
+          
+          <center>
+          <Box background="#1f80e0" py="4px" >
+                <Text>
+                Subscribe to watch all content 
+                </Text>  
+                </Box>
+                </center>
+                </Box>
+
       
-      <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-      <Box p="6">
-        <Box d="flex" alignItems="baseline">
-          <Text fontSize="xl" fontWeight="semibold" color="teal.600">
+      <Box p="12" >
+        <Box  className='cards11' d="flex "  rowGap="5" alignItems="center" background="#192133" p="10px 10px 12px 12px" maxW="520px" mb="12px" w="980px">
+          <Text fontSize="xl" fontWeight="semibold" >
           View Transactions
           </Text>
+          <img src="https://img.icons8.com/sf-ultralight-filled/25/000000/chevron-right.png"/>    
         </Box>
-        <Text mt="2" color="gray.500">
-        Account Settings        </Text>
-        <Text mt="2" color="gray.500">
-        Log Out        </Text>
+                     <Box  className='cards11' d="flex "  rowGap="5" alignItems="center" background="#192133" p="10px 10px 12px 12px" maxW="520px" mb="12px" w="980px">
+        <Text fontSize="xl" fontWeight="semibold" color="teal.600">       
+                 Account Settings 
+          </Text>
+          <img src="https://img.icons8.com/sf-ultralight-filled/25/000000/chevron-right.png"/>    
+
+        </Box>
+       <Box onClick={()=>{
+        auth.signOut().then(()=>{
+            dispatch(setSignOut())
+            history.push('/login')
+        })
+       }}
+          className='cards11' d="flex "  rowGap="5" alignItems="center" background="#192133" p="10px 10px 12px 12px" maxW="520px" mb="12px" w="980px">
+        <Text fontSize="xl" fontWeight="semibold" color="teal.600">
+                Log Out   
+         </Text>
+         <img src="https://img.icons8.com/sf-ultralight-filled/25/000000/chevron-right.png"/>    
+
+        </Box>
+
       </Box>
-      
-    </Box>
+      </Box>
+    
       </Container>
     </Center>
     </>
