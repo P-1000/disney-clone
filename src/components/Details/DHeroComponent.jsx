dea78423a2a8f144f81c45b092e602ea787a9235
pavanpatchikarla <76902617+P-1000@users.noreply.github.com>
2023-11-15 01:43:39 +0530

    small md design

--- src/components/Details/DHeroComponent.jsx
+++ src/components/Details/DHeroComponent.jsx
@@ -1,4 +1,4 @@
-import React,{useState } from "react";
+import React, { useState } from "react";
 import { CgMathPlus } from "react-icons/cg";
 import { FaPlay } from "react-icons/fa";
 import PlayerModal from "./Player/PlayerModal";
@@ -7,7 +7,7 @@ import { useParams } from "react-router-dom";
 
 const DHeroComponent = (props) => {
   const Back_Url = "https://image.tmdb.org/t/p/original";
-  // modal logic bro : 
+  // modal logic bro :
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
@@ -25,18 +25,84 @@ const DHeroComponent = (props) => {
   //more like this logic bro :
 
   return (
-    <div className=" rounded-xl  py-8">
-    <div>
-    <PlayerModal 
-    isOpen={isModalOpen} 
-    closeModal={closeModal}
-    id={data.id}
-    type={type}
-     />
-    </div>
-      <div className="mx-9 mb-4">
+    <div className=" rounded-xl  py-4 lg:py-8">
+      <div>
+        <PlayerModal
+          isOpen={isModalOpen}
+          closeModal={closeModal}
+          id={data.id}
+          type={type}
+        />
+      </div>
+      <div className="lg:mx-9 mb-4">
+        {/* small screen? */}
+        <div className="lg:hidden mx-4 mb-4">
+          <div className="relative w-full h-[32rem]  overflow-hidden rounded-lg divide-black">
+            {/* Gradient overlay */}
+            <div
+              className="absolute z-10 w-full h-full"
+              style={{
+                backgroundImage:
+                  "linear-gradient(90deg, rgb(3, 11, 23) 18.95%, rgba(5, 18, 29) 30.3%, rgba(9, 22, 34, 1) 42.3%, rgba(19, 34, 34, 0.79) 58.3%, rgba(19, 34, 34, 0.79) 58.3%, rgba(34, 34, 34, 0.2) 100%)",
+              }}
+            />
+
+            <div className="absolute z-30 flex flex-col items-center justify-start gap-4 h-full text-center text-white">
+              {/* Image at the top */}
+              <img
+                src={`${Back_Url}${data.backdrop_path}`}
+                alt="backdrop poster"
+                className="w-full h-[10rem] object-cover rounded-t-lg"
+              />
+
+              {/* Title */}
+              <h1 className="text-3xl font-extrabold">
+                {data.title || data.name}
+              </h1>
+
+              {/* Details (Runtime, Rating, etc.) */}
+              <p className="text-sm -mt-2 text-gray-300">
+                {type === "movie"
+                  ? (data.runtime / 60).toFixed(2) + "hr"
+                  : data.number_of_episodes + " Eps"}{" "}
+                • U/A • {data.vote_average}
+              </p>
+
+              {/* Watch Now button */}
+              <button
+                onClick={openModal}
+                className="w-full border bg-white/90 hover:bg-white transition-all
+           hover:scale-105 text-gray-900 px-8 font-bold
+           flex place-items-center justify-center py-2
+             rounded-md"
+              >
+                <FaPlay className="text-md mt-[1px] mx-2" />
+                <span className="text-md">Watch Now</span>
+              </button>
+
+              {/* Overview */}
+              <p className="text-sm h-fit pt-3 text-left text-gray-300">
+                {data.overview}
+              </p>
+
+              {/* Genre */}
+              <p className="text-md divide-x-2 ml-1 ">
+                {data?.genres?.map((gen) => (
+                  <span
+                    className="font-semibold text-sm -mt-10 mb-10   px-1"
+                    key={gen.id}
+                  >
+                    {gen.name}{" "}
+                  </span>
+                ))}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* // larger screen -------- */}
         <div
-          className="relative hidden w-full lg:block h-[30rem]   lf-9 mt-8 overflow-hidden rounded-lg divide-black"
+          className="relative  hidden w-full lg:block h-[30rem]   lg:lf-9 lg:mt-8 mt-4 overflow-hidden rounded-lg divide-black"
           // style={{ height: "28rem" }}
         >
           {/* gradient bro */}
@@ -53,29 +119,33 @@ const DHeroComponent = (props) => {
             <div>
               <h1
                 // onClick={handleMoviepl}
-                className="text-[3.2rem] mt-10 ml-12 w-6/12 text-extrabold">
+                className="lg:text-[3.2rem] text-3xl  mt-10 ml-4 lg:ml-12 w-6/12 text-extrabold"
+              >
                 {data.title || data.name}
               </h1>
 
-              <p className="text-lg -my-1 ml-10 mx-10 px-2  w-fit">
-              {/* { || data.number_of_episodes} hr     */}
-              {
-                type == "movie" ? (data.runtime/60).toFixed(2) + "hr" : data.number_of_episodes + " Eps"
-              } 
-              &#8226; U/A &#8226; {data.vote_average}  
-
-           
+              <p className="lg:text-lg text-sm mt-1 lg:mt-4 -my-1 ml-3 lg:ml-12 mx-10 px-2  w-fit">
+                {/* { || data.number_of_episodes} hr     */}
+                {type == "movie"
+                  ? (data.runtime / 60).toFixed(2) + "hr"
+                  : data.number_of_episodes + " Eps"}
+                &#8226; U/A &#8226; {data.vote_average}
               </p>
-              <p   className="text-lg divide-x-2  ml-8 mt-2 px-2  w-6/12 text-ellipsis overflow-hidden">
-              {data?.genres?.map((gen) => (
-                <span className=" font-bold px-2" key={gen.id}>{gen.name} </span>
-              ))}
+              <p className="lg:text-lg text-md divide-x-2 ml-1  lg:ml-10 mt-2 px-2  w-6/12 text-ellipsis overflow-hidden">
+                {data?.genres?.map((gen) => (
+                  <span
+                    className="font-semibold text-sm lg:text-md lg:font-bold px-1 lg:px-1"
+                    key={gen.id}
+                  >
+                    {gen.name}{" "}
+                  </span>
+                ))}
               </p>
               <p
                 className="text-lg pt-2 mt-3 ml-12 w-[48%] text-ellipsis overflow-hidden"
                 style={{ height: "120px" }}
               >
-               {data.overview}
+                {data.overview}
               </p>
 
               <div className="flex justify-between  mx-12 py-2 flex-row">
@@ -92,19 +162,18 @@ const DHeroComponent = (props) => {
                   </button> */}
                   <div className="flex place-items-center py-4">
                     <button onClick={openModal}>
-                      <div 
-                      className="flex border bg-white/90 hover:bg-white transition-all 
-                      hover:scale-105 text-gray-900 px-14 font-bold p-2  rounded-md">
-                        <FaPlay className="text-lg mt-1 mx-2" />  
+                      <div
+                        className="flex border bg-white/90 hover:bg-white transition-all 
+                      hover:scale-105 text-gray-900 px-14 font-bold p-2  rounded-md"
+                      >
+                        <FaPlay className="text-lg mt-1 mx-2" />
                         <h1 className="text-lg ">Watch Now</h1>
                       </div>
-                    
                     </button>
-                    <button
-                      className="mx-4 flex flex-col  bg-gray-500/40 p-1 rounded-lg  place-items-center ">
-                   <div className="p-[2px]">
-                   <CgMathPlus size={36} />
-                   </div>
+                    <button className="mx-4 flex flex-col  bg-gray-500/40 p-1 rounded-lg  place-items-center ">
+                      <div className="p-[2px]">
+                        <CgMathPlus size={36} />
+                      </div>
                       {/* <p className="-mx-10" >
                      Add to List
                     </p> */}
