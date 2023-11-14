2e5b5a0ce67e8bfc3212c2d6db819a9a4cda2fff
pavanpatchikarla <76902617+P-1000@users.noreply.github.com>
2023-11-15 01:55:04 +0530

    Ep small screen

--- src/components/Details/Episodes/EpisodeModal.jsx
+++ src/components/Details/Episodes/EpisodeModal.jsx
@@ -50,12 +50,12 @@ const EpisodeModal = ({ isOpen, closeModal, season, sid }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 p-16">
+    <div className="fixed inset-0 flex items-center justify-center z-50 p-4 sm:p-16">
       <div className="absolute inset-0 bg-black opacity-80 backdrop-filter backdrop-blur-lg"></div>
-      <div className="p-9 rounded-md flex flex-col gap-10 items-center justify-between shadow-white shadow-md z-10 max-h-full overflow-y-auto backdrop-filter backdrop-blur-lg border border-gray-300 relative">
+      <div className="p-4 sm:p-9 rounded-md flex flex-col gap-4 sm:gap-10 items-center justify-between shadow-white shadow-md z-10 max-h-full overflow-y-auto backdrop-filter backdrop-blur-lg border border-gray-300 relative">
         <div className="sticky -top-10 bg-gray-900/50 z-10 w-full">
           <button
-            className="absolute top-4 p-2 right-4 text-white cursor-pointer hover:text-red-500"
+            className="absolute top-2 sm:top-4 p-2 right-2 sm:right-4 text-white cursor-pointer hover:text-red-500"
             onClick={() => {
               closeModal();
               setSelectedEpisode(null); // Reset selected episode when closing modal
@@ -63,50 +63,51 @@ const EpisodeModal = ({ isOpen, closeModal, season, sid }) => {
           >
             <AiOutlineClose size={24} />
           </button>
-          <h2 className="text-2xl p-2 font-bold mb-4 text-white">
+          <h2 className="text-lg sm:text-2xl p-2 font-bold mb-2 sm:mb-4 text-white">
             {`Season ${season || ""} `}
           </h2>
-          <h1 className="text-xl font-bold text-white mb-4">{season?.name}</h1>
+          <h1 className="text-md sm:text-xl font-bold text-white mb-2 sm:mb-4">
+            {season?.name}
+          </h1>
         </div>
         {loading ? (
           Array.from({ length: 5 }, (_, index) => (
             <div
-              className="flex justify-between gap-2 mt-2 transition-all animate-pulse
+              className="flex flex-col gap-2 mt-2 transition-all animate-pulse
               hover:bg-gray-900/30 hover:rounded-md hover:bg-clip-padding hover:backdrop-filter
               cursor-pointer relative group"
               key={index}
             >
-              <div className="w-8/12">
-                <div className="p-5">
+              <div className="w-full">
+                <div className="p-2">
                   <Skeleton />
                   <Skeleton />
                   <Skeleton />
                 </div>
               </div>
-              <div className="w-4/12 pt-5 px-2">
-                <Skeleton />
-              </div>
             </div>
           ))
         ) : (
-          <div className="flex flex-col w-full p-2">
+          <div className="flex flex-col w-full p-2 divide-y-2 ">
             {epData.map((episode) => (
               <div
-                className="flex justify-between gap-2 mt-2
+                className="flex lg:flex-nowrap flex-wrap-reverse  whitespace-pre-wrap gap-2 mt-2
               transition-all 
               hover:bg-gray-900/30 hover:rounded-md hover:bg-clip-padding hover:backdrop-filter
                cursor-pointer relative group"
                 key={episode.id}
                 onClick={() => handleEpisodeClick(episode)}
               >
-                <div className="w-8/12">
-                  <div className="p-5">
-                    <div className="flex gap-1 text-xl font-bold">
+                <div className="w-full">
+                  <div className="p-2">
+                    <div className="flex gap-1 text-md sm:text-xl font-bold">
                       <h1>{episode.episode_number}.</h1>
                       <h1>{episode.name}</h1>
                     </div>
-                    <p className="mt-2">{episode.overview}</p>
-                    <div className="flex gap-3 mt-4 place-items-center">
+                    <p className="mt-2 text-sm sm:text-md">
+                      {episode.overview}
+                    </p>
+                    <div className="flex gap-3 mt-2 sm:mt-4 place-items-center">
                       <div className="flex items-center">
                         <FaClock className="text-gray-400 mr-1" />
                         <span>{episode.runtime} min</span>
@@ -118,20 +119,18 @@ const EpisodeModal = ({ isOpen, closeModal, season, sid }) => {
                     </div>
                   </div>
                 </div>
-                <div className="w-4/12 mt-5 px-2 relative">
+                <div className="w-full mt-2 relative">
                   <img
                     src={`https://image.tmdb.org/t/p/original/${episode.still_path}`}
                     alt={`Episode ${episode.episode_number} Poster`}
-                    className="w-full h-56 object-cover rounded-md mb-4"
+                    className="w-full h-44 sm:h-56 object-cover rounded-md mb-2 sm:mb-4"
                   />
                   <motion.div
                     initial={{ opacity: 0, scale: 1.8 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.6 }}
                   >
-                    <PlayButton
-                      onClick={() => handleEpisodeClick(episode)}
-                    />
+                    <PlayButton onClick={() => handleEpisodeClick(episode)} />
                   </motion.div>
                 </div>
               </div>
