9b5453458c556bba44c6a62f36ba832799dba546
pavanpatchikarla <76902617+P-1000@users.noreply.github.com>
2023-11-14 23:58:57 +0530

    episodes done

--- src/components/Details/Episodes/EpisodeModal.jsx
+++ src/components/Details/Episodes/EpisodeModal.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { AiOutlineClose } from "react-icons/ai";
+import { FaClock, FaStar } from "react-icons/fa";
 
 const EpisodeModal = ({ isOpen, closeModal, season, sid }) => {
   const [epData, setEpisodes] = useState([]);
@@ -13,7 +14,7 @@ const EpisodeModal = ({ isOpen, closeModal, season, sid }) => {
       const data = await res.json();
       setEpisodes(data.episodes);
       setLoading(false);
-      console.log(data.episodes);
+      console.log(data);
     } catch (error) {
       console.error("Error fetching episodes data:", error);
       setLoading(false);
@@ -31,49 +32,61 @@ const EpisodeModal = ({ isOpen, closeModal, season, sid }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 p-16">
       <div className="absolute inset-0 bg-black opacity-80 backdrop-filter backdrop-blur-lg"></div>
-      <h2 className="text-2xl font-bold mb-4 text-white">
-        {`Season ${season?.season_number || ""} Episodes`}
-      </h2>
-      <div className="p-9 rounded-md flex gap-10 justify-between shadow-white shadow-md z-10 max-h-full overflow-y-auto backdrop-filter backdrop-blur-lg border border-gray-300">
-        <div className=" flex flex-col">
+      <div className="p-9 rounded-md flex flex-col gap-10 items-center justify-between shadow-white shadow-md z-10 max-h-full overflow-y-auto backdrop-filter backdrop-blur-lg border border-gray-300 relative">
+        <div className=" sticky -top-10 bg-gray-900/50 z-10  w-full">
           <button
-            className="self-end text-white cursor-pointer hover:text-red-500"
+            className="absolute top-4 p-2 right-4 text-white cursor-pointer hover:text-red-500"
             onClick={closeModal}
           >
             <AiOutlineClose size={24} />
           </button>
-
-          {loading ? (
-            <p className="text-gray-800">Loading...</p>
-          ) : (
-            <div className="flex w-full p-2 ">
-              <div>
-                {epData.map((episode) => (
-                  <div
-                    key={episode.id}
-                    className="mb-4 flex gap-4  text-slate-400"
-                  >
-                    <div className="8/12">
-                      <h3 className="text-lg font-semibold">
-                        Episode {episode.episode_number}
-                      </h3>
-                      <p>Air Date: {episode.air_date}</p>
-                      <p>Overview: {episode.overview}</p>
-                    </div>
-                    <div className="4/12">
-                      <img
-                        key={episode.id}
-                        src={`https://image.tmdb.org/t/p/original/${episode.still_path}`}
-                        alt={`Episode ${episode.episode_number} Still`}
-                        className="lg:h-44 h-52 object-cover rounded-md mb-4 transition-transform transform hover:scale-105"
-                      />
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
+          <h2 className="text-2xl p-2 font-bold mb-4 text-white">
+            {`Season ${season || ""} `}
+          </h2>
+          <h1 className="text-xl font-bold text-white mb-4">{season?.name}</h1>
         </div>
+        {loading ? (
+          <p className="text-gray-800">Loading...</p>
+        ) : (
+          <div className="flex flex-col w-full p-2">
+            {epData.map((episode) => (
+              <div
+                className="flex justify-between gap-2  mt-2
+              transition-all 
+              hover:bg-gray-900/30 hover:rounded-md hover:bg-clip-padding hover:backdrop-filter
+               cursor-pointer"
+                key={episode.id}
+              >
+                <div className="w-8/12">
+                  <div className="p-5">
+                    <div className="flex gap-1 text-xl font-bold">
+                      <h1>{episode.episode_number}.</h1>
+                      <h1>{episode.name}</h1>
+                    </div>
+                    <p className="mt-2">{episode.overview}</p>
+                   <div className="flex gap-3 mt-4 place-items-center">
+                   <div className="flex items-center ">
+                      <FaClock className="text-gray-400 mr-1" />
+                      <span>{episode.runtime} min</span>
+                    </div>
+                    <div className="flex items-center ">
+                      <FaStar className="text-yellow-400 mr-1" />
+                      <span>{episode.vote_average}/10</span>
+                    </div>
+                   </div>
+                  </div>
+                </div>
+                <div className="w-4/12 pt-5 px-2">
+                  <img
+                    src={`https://image.tmdb.org/t/p/original/${episode.still_path}`}
+                    alt={`Episode ${episode.episode_number} Poster`}
+                    className="w-full h-52 object-cover rounded-md mb-4"
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
